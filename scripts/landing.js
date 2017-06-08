753eafc42943a54965ff0189271732cc29765421
DVS GIT TEST <vdelacuetara@gmail.com>
2017-06-08 16:46:51 -0400

    revisions from assignment 9 dom scripting events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,21 +1,20 @@
 var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function(points) {
+    var revealPoint = function(point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
 
- var revealPoint = function(point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";
-};
-
-var animatePoints = function(points) {	
 	forEach(points, revealPoint);
-   
+}
 window.onload = function () {
     
     if (window.innerHeight > 950){
         animatePoints(pointsArray);    
     }
-    
+
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
