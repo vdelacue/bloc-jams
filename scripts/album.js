a6da221d7f9d829d804d484276fdba21ee660588
Vanessa de La Cuetara <vdelacuetara@gmail.com>
2017-07-22 01:08:23 -0400

    completed checkpoint 13 play pause part 2

--- scripts/album.js
+++ scripts/album.js
@@ -47,9 +47,8 @@ var albumTwinPeaks = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
         '<tr class="album-view-song-item">'
-    +   '   <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
-    //+   '   <td class="song-item-number" data-song-number>' + songNumber + '</td>'  code corrected above from submission comment but would like explanation.
-    +   '   <td class="song-item-titel">' + songName + '</td>'
+    +   '   <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'       
+    +   '   <td class="song-item-title">' + songName + '</td>'
     +   '   <td class="song-item-duration">' + songLength + '</td>'
     +   '</tr>'
     ;
@@ -90,7 +89,7 @@ var songListContainer = document.getElementsByClassName('album-view-song-list')[
 var songRows = document.getElementsByClassName('album-view-song-item');
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-var currentPlayingSong = null;
+var currentlyPlayingSong = null;
 
 window.onload = function() {
     
@@ -109,7 +108,7 @@ window.onload = function() {
             var songItem = getSongItem(event.target);
             var songItemNumber = songItem.getAttribute('data-song-number');
             
-            if (songItemNumber !== currentlyPayingsong) {
+            if (songItemNumber !== currentlyPlayingSong) {
                 songItem.innerHTML = songItemNumber;
             }
         });
@@ -130,6 +129,7 @@ var findParentByClassName = function(element, targetClass) {
 };
 
 var getSongItem = function(element) {
+    console.log(element);
     switch (element.className) {
         case 'album-song-button':
         case 'ion-play':
@@ -148,13 +148,13 @@ var getSongItem = function(element) {
 
 var clickHandler = function(targetElement) {
     var songItem = getSongItem(targetElement);
-    if (currentPlayingSong === null) {
+    if (currentlyPlayingSong === null) {
         songItem.innerHTML = pauseButtonTemplate;
-        currentlyPayingsong = songItem.getAttribute('data-song-number')
-    } else if (currentlyPayingsong === songItem.getAttribute('data-song-number')) {
+        currentlyPlayingSong = songItem.getAttribute('data-song-number')
+    } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
         songItem.innerHTML = playButtonTemplate;
-        currentlyPayingsong = null;
-    } else if (currentlyPayingsong !== songItem.getAttribute('data-song-number')) {
+        currentlyPlayingSong = null;
+    } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
         songItem.innerHTML = pauseButtonTemplate;
