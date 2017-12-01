73b5ad1fb55245e083ca6189d4d7fbc8dda904a4
Vanessa de La Cuetara <vdelacuetara@gmail.com>
2017-12-01 16:33:59 -0500

    made mistake creating checkpoint 16 and have been working on master, will now create new branch for checkpoint 16 with these changes

--- scripts/landing.js
+++ scripts/landing.js
@@ -11,14 +11,15 @@ var animatePoints = function(points) {
 	forEach(points, revealPoint);
 };
 
-window.onload = function () {
+$(window).load = function() {
     
-    if (window.innerHeight > 950){
-        animatePoints(pointsArray);    
+    if ($(window).height() > 950) {
+        animatePoints();
     }
 
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = $('.selling-points').offset().top - $(window)
+//    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+//    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
     window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
