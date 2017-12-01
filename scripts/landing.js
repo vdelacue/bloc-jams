e24b9213f680fce9eade1b26633766d0d635a355
Vanessa de La Cuetara <vdelacuetara@gmail.com>
2017-12-01 18:20:29 -0500

    checkppoint-16-jquery-langing-page completed simplified code to jquery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,12 @@
-var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function(point) {
-        point.style.opacity = 1;
-        point.style.transform = "scaleX(1) translateY(0)";
-        point.style.msTransform = "scaleX(1) translateY(0)";
-        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+         opacity: 1,
+         transform: 'scaleX(1) translate(0)'
+        });   
+    };
+    $.each($('point'), revealPoint);
 };
 
 var animatePoints = function(points) {
@@ -17,17 +19,13 @@ $(window).load = function() {
         animatePoints();
     }
 
-    var scrollDistance = $('.selling-points').offset().top - $(window)
-//    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-//    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
-}
-
+});
 
 
 
